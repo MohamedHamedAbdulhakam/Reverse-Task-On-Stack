9b06b2888fc8b827622e321f0b4d0d1c00871ee4
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-11-10 23:57:24 +0200

    Add project files.

---
+++ Reverse task on_stack/Reverse task on_stack.cpp
@@ -0,0 +1,39 @@
+#include <iostream>  
+#include <stack>     
+#include <string>    
+
+using namespace std;
+
+int main() {
+
+    string inputStr = "Hello World";
+    stack<char> stack;
+    string result = "";
+    int i = 0;
+    while (i < inputStr.size()) {
+        char ch = inputStr[i];
+        if (ch != ' ') {
+            stack.push(ch);
+        }
+        else {
+
+            while (!stack.empty()) {
+                result += stack.top();
+                stack.pop();
+            }
+            result += ' ';
+        }
+        i++;
+        while (!stack.empty()) {
+            result += stack.top();
+            stack.pop();
+        }
+    }
+
+
+
+
+    cout << "Reversed string  :" << result << endl;
+
+    return 0;
+}
